4cc3c7953c1bec236f1995eb782ea8faba3053f0
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2020-03-18 15:22:51 +0900

    不具合対応（windowsBackgroundにより表示不正） テーマ交代処理追加

--- app/src/main/java/com/example/android_change_theme/MainActivity.java
+++ app/src/main/java/com/example/android_change_theme/MainActivity.java
@@ -6,16 +6,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String theme = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
-        String theme  = data.getString("theme", "AppTheme" );
+        theme  = data.getString("theme", "AppTheme" );
         if(theme.equals("AppTheme_Dark"))
         {
             setTheme(R.style.AppTheme_Dark);
@@ -30,17 +32,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickthemeButton(View view)
     {
-        String theme = "AppTheme_Dark";
+        String ChangeTheme ="";
+
+        if(theme.equals("AppTheme_Dark"))
+        {
+            ChangeTheme = "AppTheme";
+        }else {
+            ChangeTheme = "AppTheme_Dark";
+        }
 
         // 選択したテーマの保存処理
-        changeTheme(theme);
+        changeTheme(ChangeTheme);
     }
 
     public void changeTheme(String theme)
     {
         SharedPreferences data = getSharedPreferences("DataSave", Context.MODE_PRIVATE);
         SharedPreferences.Editor e = data.edit();
-        e.putString("theme", "AppTheme_Dark");
+        e.putString("theme", theme);
         e.apply();
     }
 }
